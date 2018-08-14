de517f247e73e8e2ea9a2fde2683ba219d07454b
prasadveeramallu <durga93@student.fdu.edu>
2018-08-14 12:38:11 -0400

    first commit

---
+++ src/com/dss/Durga.java
@@ -0,0 +1,24 @@
+package com.dss;
+
+public class Durga {
+    static int  n=10;
+    public static void main(String[]args)
+    {
+        System.out.println("Fairleign Diskinson ");
+        System.out.println(mult(5,10));
+        System.out.println(div(5,10));
+
+    }
+     int add(int a,int b)
+    {
+        return a+b;
+    }
+    static int mult(int a,int b)
+    {
+        return a*b;
+    }
+    static int div(int a,int b)
+    {
+        return a%b;
+    }
+}
